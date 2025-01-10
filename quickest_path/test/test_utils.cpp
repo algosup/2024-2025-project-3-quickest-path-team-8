bbfd41fafeece018b873ee9eee2a9dd5763b72fc
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2025-01-10 12:28:02 +0100

    First initialization of ci/cd

---
+++ quickest_path/test/test_utils.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "../src/utils.h"
+
+// Sample test for a function in utils.h
+TEST(UtilsTest, AddFunctionWorks) {
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(-1, 1), 0);
+    EXPECT_EQ(add(0, 0), 0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
